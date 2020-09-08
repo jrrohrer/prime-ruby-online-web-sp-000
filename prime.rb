e15f51a54b5559f9664c155f457d4e4cf4b41354
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-08 15:37:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 
 def prime?(num)
-  if num <= 1 return false
-  elsif num == 2 return true
+  if num <= 1 
+    return false
+  elsif num == 2 
+    return true
   else
     (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
   end
