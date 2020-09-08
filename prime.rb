513c865541c1ebb5be3c838cd646d9689758acab
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-08 15:36:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 
 def prime?(num)
-  (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
+  if num <= 1 return false
+  elsif num == 2 return true
+  else
+    (2..Math.sqrt(num)).each { |i| return false if num % i == 0}
+  end
   true
 end
